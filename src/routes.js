e7b00fff1a6b525dc027d07b643f3f611c6c020f
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-03 13:44:35 -0300

    Estilizando o Cadastro de Produtos

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,12 @@ routes.get('/', function(req, res) {
 })
 
 routes.get('/products/create', ProductController.create)
+routes.post('/products', ProductController.post)
 
+// Alias
 routes.get('/ads/create', function(req, res) {
 return res.redirect('/products/create')
 })
+
 
 module.exports = routes
