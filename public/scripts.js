73c81a6ade00883b8eecdfa96b550f107f5d465c
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-06 14:00:17 -0300

    Gerenciador de Envio de Imagens JS

--- public/scripts.js
+++ public/scripts.js
@@ -13,3 +13,37 @@ const Mask = {
       }).format(value/100)
   }
 }
+
+const PhotosUpload = {
+    uploadLimit: 6,
+    handleFileInput(event) {
+        const { files: fileList } = event.target
+        const { uploadLimit } = PhotosUpload
+
+        if (fileList.length > uploadLimit) {
+            alert(`Envie no máximo ${uploadLimit} fotos`)
+            event.preventDefault()
+            return
+        }
+
+        Array.from(fileList).forEach(file => {
+            const reader = new FileReader()
+
+            reader.onload = () => {
+                const image = new Image()
+                image.src = String(reader.results)
+
+                const div = document.createElement('div')
+                div.classList.add('photo')
+
+                div.onclick = () => alert('remover foto')
+
+                div.appendChild(image)
+
+                document.querySelector('#photos-preview').appendChild(div)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    }
+}
