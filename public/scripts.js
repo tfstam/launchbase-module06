3addba951a4d159af0b0c7c0f96af3c98f6d70d2
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-06 14:46:59 -0300

    Removendo Imagens do Gerenciador

--- public/scripts.js
+++ public/scripts.js
@@ -15,35 +15,62 @@ const Mask = {
 }
 
 const PhotosUpload = {
-    uploadLimit: 6,
-    handleFileInput(event) {
-        const { files: fileList } = event.target
-        const { uploadLimit } = PhotosUpload
+  preview: document.querySelector('#photos-preview'), 
+  uploadLimit: 6,
+  handleFileInput(event) {
+    const { files: fileList } = event.target
+    
+    if (PhotosUpload.hasLimit(event)) return
 
-        if (fileList.length > uploadLimit) {
-            alert(`Envie no máximo ${uploadLimit} fotos`)
-            event.preventDefault()
-            return
+    Array.from(fileList).forEach(file => {
+        const reader = new FileReader()
+
+        reader.onload = () => {
+            const image = new Image()
+            image.src = String(reader.result)
+
+            const div = PhotosUpload.getContainer(image)
+
+            PhotosUpload.preview.appendChild(div)
         }
 
-        Array.from(fileList).forEach(file => {
-            const reader = new FileReader()
+        reader.readAsDataURL(file)
+    })
+  },
+  hasLimit(event) {
+    const { uploadLimit } = PhotosUpload
+    const { files: fileList } = event.target
 
-            reader.onload = () => {
-                const image = new Image()
-                image.src = String(reader.results)
-
-                const div = document.createElement('div')
-                div.classList.add('photo')
-
-                div.onclick = () => alert('remover foto')
-
-                div.appendChild(image)
-
-                document.querySelector('#photos-preview').appendChild(div)
-            }
-
-            reader.readAsDataURL(file)
-        })
+    if (fileList.length > uploadLimit) {
+        alert(`Envie no máximo ${uploadLimit} fotos`)
+        event.preventDefault()
+        return true
     }
+
+    return false
+  },
+  getContainer(image) {
+    const div = document.createElement('div')
+    div.classList.add('photo')
+
+    div.onclick = PhotosUpload.removePhoto
+
+    div.appendChild(image)
+    div.appendChild(PhotosUpload.getRemoveButton())
+
+    return div
+  },
+  getRemoveButton() {
+    const button = document.createElement('i')
+    button.classList.add('material-icons')
+    button.innerHTML = 'close'
+    return button
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode
+    const photosArray = Array.from(PhotosUpload.preview.children)
+    const index = photosArray.indexOf(photoDiv)
+
+    photoDiv.remove()
+  }
 }
