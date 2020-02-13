3a3d22afe301c1f391ce090dc708f939c315f657
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-13 15:12:03 -0300

    Exclusão de Imagens do BD e Gerenciador

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -67,9 +67,19 @@ module.exports = {
     const keys = Object.keys(req.body)
 
     for(let key of keys) {
-      if (req.body[key] == "") {
+      if (req.body[key] == "" && key != "removed_files") {
         return res.send('Please, fill all fields!')
       }
+    }
+
+    if (req.body.removed_files) {
+      const removedFiles = req.body.removed_files.split(',')
+      const lastIndex = removedFiles.length - 1
+      removedFiles.splice(lastIndex, 1)
+
+      const removedFilesPromise = removedFiles.map(id => File.delete(id))
+
+      await Promise.all(removedFilesPromise)
     }
 
     req.body.price = req.body.price.replace(/\D/g, "")
