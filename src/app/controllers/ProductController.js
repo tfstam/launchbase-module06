fe6088eb86c35af707a83636145e960f75ca5221
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-13 13:04:15 -0300

    Apresentando Imagens no Gerenciador de Imagens

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -49,10 +49,19 @@ module.exports = {
     product.old_price = formatPrice(product.old_price)
     product.price = formatPrice(product.price)
 
+    // get categories
     results = await Category.all()
     const categories = results.rows
 
-    return res.render('products/edit.njk', { product, categories })
+    // get images
+    results = await Product.files(product.id)
+    let files = results.rows
+    files = files.map(file => ({
+      ...file,
+      src: `${req.protocol}://${req.headers.host}${file.path.replace('public', '')}`
+    }))
+
+    return res.render('products/edit.njk', { product, categories, files })
   },
   async put(req, res) {
     const keys = Object.keys(req.body)
