3fa8b1a645b25a2d6b0402f8efb2e5510568ab92
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-17 14:10:46 -0300

    Estrutura HTML e CSS da Página de Compra

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -39,6 +39,9 @@ module.exports = {
 
     return res.redirect(`/products/${productId}/edit`)
   },
+  show(req, res) {
+    return res.render('products/show.njk')
+  },
   async edit(req, res) {
 
     let results = await Product.find(req.params.id)
