25b5f05cc005ab28f88992d58ff7fe963e9ae9b1
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-17 14:54:40 -0300

    Pegando Dados para Apresentação da Página

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -1,4 +1,4 @@
-const { formatPrice } = require('../../lib/utils')
+const { formatPrice, date } = require('../../lib/utils')
 
 const Category = require('../models/Category')
 const Product = require('../models/Product')
@@ -39,8 +39,24 @@ module.exports = {
 
     return res.redirect(`/products/${productId}/edit`)
   },
-  show(req, res) {
-    return res.render('products/show.njk')
+  async show(req, res) {
+
+    let results = await Product.find(req.params.id)
+    const product = results.rows[0]
+
+    if (!product) return res.send('Product not found!')
+
+    const { day, hour, minutes, month } = date(product.updated_at)
+
+    product.published = {
+      day: `${day}/${month}`,
+      hour: `${hour}h${minutes}`
+    }
+
+    product.oldPrice = formatPrice(product.old_price)
+    product.price = formatPrice(product.price)
+
+    return res.render('products/show.njk', { product })
   },
   async edit(req, res) {
 
@@ -100,7 +116,7 @@ module.exports = {
 
     await Product.update(req.body)
 
-    return res.redirect(`/products/${req.body.id}/edit`)
+    return res.redirect(`/products/${req.body.id}`)
   },
   async delete(req, res) {
     await Product.delete(req.body.id)
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -8,10 +8,16 @@ module.exports = {
 
     const day = `0${date.getUTCDate()}`.slice(-2)
 
+    const hour = date.getHours()
+
+    const minutes = date.getMinutes()
+
     return {
       day,
       month,
       year,
+      hour,
+      minutes,
       iso: `${year}-${month}-${day}`,
       birthDay: `${day}/${month}`,
       format: `${day}/${month}/${year}`
