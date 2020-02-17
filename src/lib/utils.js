846ead58bdd614f6283db5f0920ee870151b99aa
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-17 15:19:14 -0300

    Procedures e Triggers SQL

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,11 +2,11 @@ module.exports = {
   date(timestamp) {
     const date = new Date(timestamp)
 
-    const year = date.getUTCFullYear()
+    const year = date.getFullYear()
 
-    const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+    const month = `0${date.getMonth() + 1}`.slice(-2)
 
-    const day = `0${date.getUTCDate()}`.slice(-2)
+    const day = `0${date.getDate()}`.slice(-2)
 
     const hour = date.getHours()
 
