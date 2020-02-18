67b2ac9540fe8f38ca684b21b617487150305624
Tamires Santos <tfs.tamsantos@gmail.com>
2020-02-18 09:51:50 -0300

    Funcionalidade da Galeria de Imagens do Produto

--- public/scripts.js
+++ public/scripts.js
@@ -117,3 +117,16 @@ const PhotosUpload = {
     photoDiv.remove()
   }
 }
+
+const ImageGallery = {
+  highlight: document.querySelector('.gallery .highlight > img'),
+  previews: document.querySelectorAll('.gallery-preview img'),
+  setImage(e) {
+    const { target } = e
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+    target.classList.add('active')
+
+    ImageGallery.highlight.src = target.src
+  }
+}
